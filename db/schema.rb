73ac495f9b640a8e1d11a2905bf6be4ee9f8aa37
Silviu D. Bojica <bojicas@gmail.com>
2008-11-05 01:03:06 +0400

    Watching Railscast, I have noticed the presence of *_updated_at column in the migration, so here I come and added myself to the application and I'm happy that it works. Here are the command line command I have issued:
    ./script/generate migration AddAvatarUpdatedAtToUser avatar_updated_at:datetime
    
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081104103151) do
+ActiveRecord::Schema.define(:version => 20081104205529) do
 
   create_table "efforts", :force => true do |t|
     t.text     "body"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20081104103151) do
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
