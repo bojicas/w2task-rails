4b66b16a23b3195f923ec24aa9c42b77a3886b87
Silviu D. Bojica <bojicas@me.com>
2008-11-09 15:23:22 +0400

    public/avatars goes now as a link to a shared folder.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   # include AuthenticatedSystem
-  before_filter :login_required, :except => 'new'
+  before_filter :login_required, :except => ['new', 'create']
   
   # render new.rhtml
   def new
