c1676ccb80397e09081da1f88f84bc5980750b39
Silviu D. Bojica <bojicas@me.com>
2008-11-11 20:51:38 +0400

    Helper to transform minutes in hh:mm (#25)

--- app/helpers/efforts_helper.rb
+++ app/helpers/efforts_helper.rb
@@ -1,3 +1,13 @@
 module EffortsHelper
-
+  def hh_mm(start, stop)
+    stop ||= Time.now
+    #stop-start
+    hh = ((stop - start) / 3600).to_i
+    mm = (hh + 1) * 60 - ((stop - start) / 60).to_i
+    if hh > 0
+      hh.to_s + 'h ' + mm.to_s + 'm'
+    else
+      mm.to_s + 'm'
+    end
+  end
 end
--- app/models/effort.rb
+++ app/models/effort.rb
@@ -2,9 +2,4 @@ class Effort < ActiveRecord::Base
   
   belongs_to :user
   
-  def minutes(start, stop)
-    stop ||= Time.now
-    ((stop - start) / 60).to_i
-  end
-  
 end
