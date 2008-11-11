03b7d76528ba23b69b60c245a2081b27122c20e1
Silviu D. Bojica <bojicas@me.com>
2008-11-11 21:03:24 +0400

    Correct a calculation error in EffortsHelper:hh_mm

--- app/helpers/efforts_helper.rb
+++ app/helpers/efforts_helper.rb
@@ -3,7 +3,7 @@ module EffortsHelper
     stop ||= Time.now
     #stop-start
     hh = ((stop - start) / 3600).to_i
-    mm = (hh + 1) * 60 - ((stop - start) / 60).to_i
+    mm = ((stop - start) / 60).to_i - hh * 60
     if hh > 0
       hh.to_s + 'h ' + mm.to_s + 'm'
     else
