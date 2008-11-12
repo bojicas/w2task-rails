f796f7759f7f07479bc137dd75467ed0b1c6d08a
Silviu D. Bojica <bojicas@me.com>
2008-11-12 17:50:06 +0400

    Stepping forward to a more clear effort list.

--- app/controllers/efforts_controller.rb
+++ app/controllers/efforts_controller.rb
@@ -4,11 +4,6 @@ class EffortsController < ApplicationController
   def index
     @efforts_today = Effort.find(:all, 
       :order => "start DESC, stop DESC, body", 
-      :conditions => "start >= '#{Time.today.to_s(:db)}'",
-      :include => :user)
-    @efforts_yestarday = Effort.find(:all, 
-      :order => "start DESC, stop DESC, body", 
-      :conditions => { :start => (1.day.ago(Time.today))...(Time.today-1) },
       :include => :user)
 
     respond_to do |format|
