4b12e4836faf0712f3d83130d98e07bdbb508fcb
Silviu D. Bojica <bojicas@me.com>
2008-11-28 00:38:48 +0400

    Ticket #44 setting image_magick_path to /opt/local/bin

--- config/initializers/paperclib.rb
+++ config/initializers/paperclib.rb
@@ -1,5 +1,5 @@
-if RAILS_ENV == "development" || RAILS_ENV == "test"
-  Paperclip.options[:image_magick_path] = '/opt/local/bin/' 
-#else 
+# if RAILS_ENV == "development" || RAILS_ENV == "test"
+Paperclip.options[:image_magick_path] = '/opt/local/bin/' 
+# else 
 #  Paperclip.options[:image_magick_path] = '/usr/bin/' 
-end 
+# end 
