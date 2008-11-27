7fb5265a822842a481e5ad8bf5cae39d64b5ed76
Silviu D. Bojica <bojicas@me.com>
2008-11-27 23:00:35 +0400

    ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,4 +49,4 @@ namespace :deploy do
 end
 
 after 'deploy:update_code', 'deploy:symlink_shared'
-after 'deploy:symlink_shared', 'fix_script_perms'
+after 'deploy:symlink_shared', 'deploy:fix_script_perms'
