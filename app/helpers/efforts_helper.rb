1f6ed3153d3f15997d5a894b2640367e5e5614d9
Silviu D. Bojica <bojicas@me.com>
2008-12-22 17:17:04 +0400

    Timesheet for current month is now in an working status.

--- app/helpers/efforts_helper.rb
+++ app/helpers/efforts_helper.rb
@@ -10,4 +10,24 @@ module EffortsHelper
       mm.to_s + 'm'
     end
   end
+  
+  def effort_in_day(user_id, project_id = nil, year = Date.today.year, month = Date.today.month, day = Date.today.day)
+    find_date = Date.new(year, month, day)
+    
+    if project_id
+      efforts = Effort.find(:all,
+        :conditions => { :user_id => user_id, 
+          :start => find_date..find_date+1,
+          :project_id => project_id
+        }
+      )
+    else
+      efforts = Effort.find(:all,
+        :conditions => { :user_id => user_id, 
+          :start => find_date..find_date+1
+        }
+      )
+    end
+    efforts.size
+  end
 end
