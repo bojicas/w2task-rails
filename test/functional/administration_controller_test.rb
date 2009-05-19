a8034d0c0f706ab4ce5ae25b66543a5843968859
Silviu D. Bojica <bojicas@me.com>
2009-05-19 09:10:32 +0400

    Code Cleanup Moved a step further in administration_controller_test.rb, did some code cleanup in css-es using the RubyMine inspectors, removed the wair site, as it was wrongly implemented.

--- test/functional/administration_controller_test.rb
+++ test/functional/administration_controller_test.rb
@@ -3,12 +3,18 @@ require 'test_helper'
 class AdministrationControllerTest < ActionController::TestCase
   setup :login_quentin
   
-  test "should show all businesses to witch current user is part of" do
-    #current_user = users.find_by_id(session[:user_id])
-    assert true
+  test "should show all businesses and only those to which logged in user is part of" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:businesses) # only in this particular case, and particular fixtures
+    assert_equal(users(:quentin).businesses, assigns(:businesses))
+    # if bojicas is a business which quentin (current user) can access the fail
+    assert_not_equal(businesses('bojicas'), assigns(:businesses)[0])
   end
   
-  test "should show all businesses of the current user" do
-    assert true
+  test"should show all users and only those assigned to the current business" do
+    get :index
+    assert_response :success
+    # TODO: not finished yet
   end
 end
